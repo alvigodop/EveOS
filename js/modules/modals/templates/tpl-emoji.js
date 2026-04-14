3c1a2869b6678b136c1d54a237a0b366ee324fdd
alvin <alvin@example.com>
2026-04-13 21:38:15 -0400

    feat: implement categorized emoji picker with tabbed navigation

--- js/modules/modals/templates/tpl-emoji.js
+++ js/modules/modals/templates/tpl-emoji.js
@@ -2,11 +2,12 @@
 window.modalTemplate += `
 <!-- EMOJI PICKER -->
 <div id="emoji-picker-container" class="modal-overlay" style="z-index: 4000;">
-    <div class="modal" style="width: 300px;">
+    <div class="modal" id="emoji-picker-modal">
         <div class="emoji-header">
             <h3>Pick an Emoji</h3>
             <span class="emoji-close" onclick="closeEmojiModal()">✖</span>
         </div>
+        <div id="emoji-tabs" class="emoji-tabs"></div>
         <div id="emoji-grid" class="emoji-grid"></div>
     </div>
 </div>
